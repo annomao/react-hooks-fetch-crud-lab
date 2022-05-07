d14c828222ce6a4f0d2912705af39f54af86ec92
annomao <omaoannkerubo@gmail.com>
2022-05-07 19:27:36 +0300

    add new question and persist to db

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,23 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    let postData = {
+      prompt: formData.prompt,
+      answers: [formData.answer1,formData.answer2,formData.answer3,formData.answer4],
+      correctIndex: parseInt(formData.correctIndex) 
+    }
+
+    fetch("http://localhost:4000/questions",{
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json" 
+      },
+      body:JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+    })
   }
 
   return (
